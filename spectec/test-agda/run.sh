c4f96eb7855d4f175bfba43b30dbef4543a1e373
Matija Pretnar <matija@pretnar.info>
2023-05-09 14:56:49 +0100

    Update tests

--- spectec/test-agda/run.sh
+++ spectec/test-agda/run.sh
@@ -5,7 +5,7 @@ cp -r ../spec minispec
 cd minispec && patch -s -p0 -i minispec.patch  --read-only=ignore && cd ..
 
 # Compile the spec to Agda
-../src/exe-watsup/main.exe --agda --sub --totalize minispec/*.watsup -o "$1"
+../src/exe-watsup/main.exe --agda --sub --totalize --sideconditions minispec/*.watsup -o "$1"
 
 # Run Agda
 # The `sed` incantation is needed to remove (user-specific) absolute paths in Agda output.
