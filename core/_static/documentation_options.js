4e5cace53c26debf43c808d0df4c77dc751347a1
702fbtngus <702fbtngus@users.noreply.github.com>
2025-01-14 06:23:05 +0000

    deploy: d8e33e6dc7274a695d8cafb4a85ec0c72bd6ad30

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0 (Draft 2025-01-13)',
+    VERSION: '3.0 (Draft 2025-01-14)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
