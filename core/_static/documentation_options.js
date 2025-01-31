6a9d8ddabdb88b8a383892b2573bf28223207d47
ShinWonho <ShinWonho@users.noreply.github.com>
2025-01-31 04:51:11 +0000

    deploy: 85927b5ae4fd9993c6b53569988bb1f6038f8aaa

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0 (Draft 2025-01-29)',
+    VERSION: '3.0 (Draft 2025-01-31)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
